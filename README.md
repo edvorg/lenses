46253461c94a1852bd36fda872bf568edbecc537
Edward Knyshov <edvorg@gmail.com>
2018-02-08 19:26:14 +0700

    allow zooming into non clojure.lang.IObj derived nodes

--- README.md
+++ README.md
@@ -13,33 +13,38 @@ Simple proof-of-concept lenses implementation in Clojure
                                            in
                                            out
                                            out-all
-                                           map-lens]]))
+                                           map-lens
+                                           ->
+                                           ->>]]))
 
 (-> {:data {:foo [1 2 3]
             :bar {:baz 1
                   :qux 2}}}
 
-    (in :data) ;; lens into data
+    (in :data) ;; zoom into data
 
-    (in :foo) ;; lens into :foo
+    (in :foo) ;; zoom into :foo
     (conj 4) ;; add 4 to [1 2 3]
-    out ;; lens out
+    (in 0) ;; zoom into first element of array
+    (+ 5) ;; 5 to first element of array
+    out ;; zoom out
+    out ;; zoom out
 
-    (in :bar) ;; lens into :bar
+    (in :bar) ;; zoom into :bar
     (assoc :tux 3) ;; add :tux -> 3 mapping
-    lens-values ;; lens into values
+    lens-values ;; zoom into values
     (map-lens inc) ;; increment all values
-    out ;; lens out
+    out ;; zoom out
 
     lens-keys ;; lens in keys
     (map-lens name) ;; convert all keywords to strings
-    out-all ;; lens outmost
+    out-all ;; zoom outmost
     )
 
-    => {:data {:foo [1 2 3 4]
-        :bar  {"baz" 2
-               "qux" 3
-               "tux" 4}}}
+    => {:data {:foo [6 2 3 4]
+        :bar {"baz" 2
+              "qux" 3
+              "tux" 4}}}
 ```
 
 ## License
--- src/rocks/clj/lenses/core.clj
+++ src/rocks/clj/lenses/core.clj
@@ -1,45 +1,45 @@
 (ns rocks.clj.lenses.core)
 
+(def ^:dynamic *backtrack* (list))
+
 (defn lens-values [m]
-  (let [{:keys [out]} (meta m)]
-    (with-meta
-      (vals m)
-      {:out (conj out (fn [v]
-                        (with-meta
-                          (zipmap (keys m) v)
-                          {:out out})))})))
+  (swap! *backtrack* conj (fn [v]
+                            (zipmap (keys m) v)))
+  (vals m))
 
 (defn lens-keys [m]
-  (let [{:keys [out]} (meta m)]
-    (with-meta
-      (keys m)
-      {:out (conj out (fn [v]
-                        (with-meta
-                          (zipmap v (vals m))
-                          {:out out})))})))
+  (swap! *backtrack* conj (fn [v]
+                            (zipmap v (vals m))))
+  (keys m))
 
 (defn in [m k]
-  (let [{:keys [out]} (meta m)]
-    (with-meta
-      (get m k)
-      {:out (conj out (fn [v]
-                        (with-meta
-                          (assoc m k v)
-                          {:out out})))})))
+  (swap! *backtrack* conj (fn [v]
+                            (assoc m k v)))
+  (get m k))
 
 (defn out [v]
-  (let [{[last-out] :out} (meta v)]
+  (let [[last-out] @*backtrack*]
     (if-not last-out
       v
-      (last-out v))))
+      (do
+        (swap! *backtrack* rest)
+        (last-out v)))))
 
 (defn out-all [v]
-  (let [{[last-out] :out} (meta v)]
+  (let [[last-out] @*backtrack*]
     (if-not last-out
       v
-      (out-all (last-out v)))))
+      (do
+        (swap! *backtrack* rest)
+        (out-all (last-out v))))))
 
 (defn map-lens [coll f]
-  (with-meta
-    (map f coll)
-    (meta coll)))
+  (map f coll))
+
+(defmacro -> [& forms]
+  `(binding [*backtrack* (atom ())]
+     (clojure.core/-> ~@forms)))
+
+(defmacro ->> [& forms]
+  `(binding [*backtrack* (atom ())]
+     (clojure.core/->> ~@forms)))
